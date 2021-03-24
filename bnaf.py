76359ebdc9a6c29b5d3c72d591c4e691d1e7cd56
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2021-03-24 22:06:45 +0800

    first commit

--- bnaf.py
+++ bnaf.py
@@ -217,7 +217,9 @@ class MaskedWeight(torch.nn.Module):
         w, wpl = self.get_weights()
         
         g = wpl.transpose(-2, -1).unsqueeze(0).repeat(inputs.shape[0], 1, 1, 1)
-        
+        print(inputs.shape)
+        print(w.shape)
+        print('--------')
         return inputs.matmul(w) + self.bias, torch.logsumexp(
             g.unsqueeze(-2) + grad.transpose(-2, -1).unsqueeze(-3), -1) if grad is not None else g
 
