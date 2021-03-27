5fd0f39de43265d6890e39b4ce65d8cf67af24fe
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2021-03-27 22:02:14 +0800

    test vdo

--- lognormal.py
+++ lognormal.py
@@ -210,7 +210,7 @@ def main():
 
     parser.add_argument('--flows', type=int, default=1)
     parser.add_argument('--layers', type=int, default=1)
-    parser.add_argument('--hidden_dim', type=int, default=8)
+    parser.add_argument('--hidden_dim', type=int, default=2)
 
     parser.add_argument('--expname', type=str, default='')
     parser.add_argument('--load', type=str, default=None)
@@ -274,7 +274,7 @@ def main():
 
     results = test_density1d(model,dataloader,args)
 
-    # np.savetxt('lognormal-100-layer3-nodes-256-trdata3m.txt', results.detach().cpu().numpy(),  fmt='%.18f')
+    np.savetxt('lognormal-100-layer1-nodes-2-trdata3m.txt', results.detach().cpu().numpy(),  fmt='%.18f')
 
     # if args.save:
     #     print('Saving..')
