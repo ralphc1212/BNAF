851f7e37689777c274cc8b07039b33193ca995e5
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2021-03-27 18:37:13 +0800

    test vdo

--- lognormal.py
+++ lognormal.py
@@ -207,7 +207,7 @@ def main():
 
     parser.add_argument('--flows', type=int, default=1)
     parser.add_argument('--layers', type=int, default=3)
-    parser.add_argument('--hidden_dim', type=int, default=256)
+    parser.add_argument('--hidden_dim', type=int, default=16)
 
     parser.add_argument('--expname', type=str, default='')
     parser.add_argument('--load', type=str, default=None)
