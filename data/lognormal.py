b6f9e790ffe22bebce95a4e3f9f7bf62140b6369
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2021-03-28 14:50:10 +0800

    test vdo

--- data/lognormal.py
+++ data/lognormal.py
@@ -7,7 +7,7 @@ class data_lognormal:
         # with open(location+'/lognormal_100.out', 'r') as f:
         #     lines = f.readlines()
 
-        x = np.loadtxt(location+'/lognormal_100.out')
+        x = np.loadtxt(location+'/lognormal_100.out', dtype=np.double)
         self.all = torch.from_numpy(x).unsqueeze(1).double()
 
         print(len(np.unique(x)))
